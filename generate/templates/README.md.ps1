60fd03faf3d6d28b57e0ac1ba0b529883fa0614d
Leo <leonardjonathanoh@live.com>
2019-10-17 10:44:02 +0000

    Update readme with badges: gitlab-ci, github-tag, docker-image-size, docker-image-layers

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,10 @@
 @"
 # docker-ansible
 
-[![pipeline status](https://gitlab.com/leojonathanoh/docker-ansible/badges/dev/pipeline.svg)](https://gitlab.com/leojonathanoh/docker-ansible/commits/dev)
+[![gitlab-ci](https://img.shields.io/gitlab/pipeline/leojonathanoh/docker-ansible/dev)](https://gitlab.com/leojonathanoh/docker-ansible/commits/dev)
+[![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-ansible)](https://github.com/leojonathanoh/docker-ansible/releases/)
+[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-ansible/latest)](https://hub.docker.com/r/leojonathanoh/docker-ansible)
+[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-ansible/latest)](https://hub.docker.com/r/leojonathanoh/docker-ansible)
 
 Dockerized ``ansible`` alpine image with some optional tools
 
