07062caa887e4309298dd0c8c768e35f7e0340b0
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-01-11 08:05:22 +0000

    Fix: Fix github actions badge in readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-ansible
 
-[![github-actions](https://github.com/leojonathanoh/docker-ansible/workflows/build/badge.svg)](https://github.com/leojonathanoh/docker-ansible/actions)
+[![github-actions](https://github.com/leojonathanoh/docker-ansible/workflows/ci-master-pr/badge.svg)](https://github.com/leojonathanoh/docker-ansible/actions)
 [![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-ansible/latest)](https://hub.docker.com/r/leojonathanoh/docker-ansible)
 [![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-ansible/latest)](https://hub.docker.com/r/leojonathanoh/docker-ansible)
 
